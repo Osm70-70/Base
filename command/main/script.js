93c3574e634a25fe3d22ef4b4c1d6e938e76d849
Osm70-70 <127863586+Osm70-70@users.noreply.github.com>
2023-03-22 02:13:38 +0000

    Delete script.js

--- command/main/script.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "script",
-  cmd: ['script','sc','source'],
-  category: 'main',
-  start: async (mom, m, { text }) => {
-    m.reply("Script Berasal Dari : https://github.com/NzrlAfndi")
-  }
-}
